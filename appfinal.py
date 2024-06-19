d150eedc2a98d80d4f132f6558b66279f1ea1d50
ejhernandybukz <131395290+ejhernandybukz@users.noreply.github.com>
2024-06-19 15:19:56 -0500

    Update appfinal.py

--- appfinal.py
+++ appfinal.py
@@ -19,9 +19,9 @@ st.sidebar.image(resized_image)
 
 
 with st.sidebar:
-    choose = option_menu("Menú de opciones", ['Actualización de inventario celesa', 'Creación de productos', 
-                                              'Corte - Ventas', 'Corte - No Ventas', 'Sugerido Inventario', 'Proveedores Stock'],
-    icons=["list check", "database up", 'envelope at', 'envelope at',"check2 square", "list check" ], menu_icon="cast", default_index=0,
+     choose = option_menu("Menú de opciones", ['Actualización de inventario celesa', 'Creación de productos', 
+                                              'Corte - Ventas', 'Corte - No Ventas', 'Sugerido Inventario', 'Pedidos', 'proveedores', 'Pedidos USD'],
+    icons=["list check", "database up", 'envelope at', 'envelope at',"check2 square",'envelope at', "list check", "coin" ], menu_icon="cast", default_index=0,
     styles={ "container": {"padding": "5!important", "background-color": "#fafafa"},
         "icon": { "font-size": "25px"}, 
         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee", "color": "black"},
@@ -53,3 +53,6 @@ elif choose == 'Sugerido Inventario':
     
 elif choose == 'Proveedores Stock':
     provedores_inventario()
+ 
+elif choose == 'Pedidos USD':
+    conversor_pedidos()
